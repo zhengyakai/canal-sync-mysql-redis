49e5b0c862fa670eca2ff187ce349f14736e0b18
zhengyakai <yakai512@163.com>
2018-12-18 17:47:32 +0800

    优化

--- src/main/resources/sql/blog.sql
+++ src/main/resources/sql/blog.sql
@@ -1,25 +1,6 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : tx-yakai
- Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : 58.87.67.162:3306
- Source Schema         : test
-
- Target Server Type    : MySQL
- Target Server Version : 50721
- File Encoding         : 65001
-
- Date: 18/12/2018 17:04:37
-*/
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for blog
--- ----------------------------
 DROP TABLE IF EXISTS `blog`;
 CREATE TABLE `blog` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
